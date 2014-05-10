421e7fc2c70f07a0f9271a6e5bd0ae2b1bf5a039
salarkhan <salar.k3@gmail.com>
2014-05-10 00:59:30 -0700

    tests passing for writing Posts to DB

--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -4,4 +4,13 @@ describe Post do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:author) }
     it { should validate_presence_of(:content) }
+
+    it "should be able to write a post to the DB" do
+        params = {
+            title: "herp",
+            author: "derp",
+            content: "meow"
+        }
+        expect{ post '/create', params }.to change{Post.count}.by(1)
+    end
 end
