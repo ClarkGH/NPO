addf9aa383c305a56214fc4c29dc8d4182400f5b
salarkhan <salar.k3@gmail.com>
2014-05-13 02:04:59 -0700

    AJAXifies CRUD & login routes     * get/displayPartial works for all routes

--- public/js/app.js
+++ public/js/app.js
@@ -11,6 +11,7 @@ function Blog( postView ) {
 Blog.prototype = {
     bindEventListeners: function(){
         $('.deena, .jennifer, .rachel').on( 'click', this.getAuthorPosts.bind( this ) )
+        $('a.login, a.edit, a.delete, a.create').on( 'click', this.getPartial.bind( this ) )
    },
 
     getAuthorPosts: function(){
@@ -31,7 +32,17 @@ Blog.prototype = {
         ajaxRequest.done( function( template ){
             self.postView.displayPost( template, postData )
         })
-    }
+    },
+
+    getPartial: function(){
+        event.preventDefault()
+        var ajaxRequest = $.ajax({
+            url: event.target.href,
+            type: 'GET'
+        })
+
+        ajaxRequest.done( this.postView.displayPartial )
+    },
 }
 
 function PostView(){
@@ -42,5 +53,9 @@ PostView.prototype = {
         var mustacheData = { data: postData }
         var rendered = Mustache.render( template, mustacheData )
         $('.container').html( rendered )
+    },
+
+    displayPartial: function( partial ){
+        $('.container').html( partial )
     }
 }
