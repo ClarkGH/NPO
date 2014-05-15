fa1dd0f5039eaf64a6997c268aa4e7974b107813
salarkhan <salar.k3@gmail.com>
2014-05-14 17:56:09 -0700

    add medium RTF editor     * .editable for medium editor needs to be a div, can't be an input     form. to get around this, i have transfer the content of the div     into the text area on form submit     * added basic CSS

--- public/js/app.js
+++ public/js/app.js
@@ -43,13 +43,24 @@ PostView.prototype = {
     },
 
     displayPost: function( postData ){
+        console.log(postData)
         var mustacheData = { data: postData }
         var template = this.getPostTemplate()
         var rendered = Mustache.render( template, mustacheData )
-        $('.container').html( rendered )
+        $('.content').html( rendered )
     },
 
     displayPartial: function( partial ){
-        $('.container').html( partial )
+        $('.content').html( partial )
+        editor = new MediumEditor('.editable', {
+            anchorInputPlaceholder: 'Type a link',
+            buttons: ['bold', 'italic', 'quote'],
+            diffLeft: 25,
+            diffTop: 10,
+            firstHeader: 'h1',
+            secondHeader: 'h2',
+            delay: 1000,
+            targetBlank: true
+        });
     }
 }
